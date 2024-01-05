bd034acfabf44ca6ad66f6311e8ca2ec848e255b
Florian Burghardt <fl.burghardt@ext.douglas.eu>
2024-01-05 15:00:14 +0100

    Remove database specific / duplicated stuff from ContentManager

--- Domain/js/1_Classes/ContentManager.js
+++ Domain/js/1_Classes/ContentManager.js
@@ -1,50 +1,35 @@
 /**
  * Class to handle all content usecases in the API-Survivor.
- * @param {object} _database - Associated database {object}.
- * @property {string} _database.name - The name of the database.
- * @property {number} _database.version - The version of the database.
- * @property {array[{object}]} _database.tables - The tables in the database.
- * @property {string} _database.tables[0].id - The id of the DOM container in the HTML document.
- * @property {string} _database.tables[0].name - The name of the table in the database.
  */
 class ContentManager
 {
 	//#region Constructor
 	/**
 	 * Constructor for the class.
-	 * @param {object} _database - Associated database {object}.
-	 * - {string} _database.name - The name of the database.
-	 * - {number} _database.version - The version of the database.
-	 * - {array[{object}]} _database.tables - The tables in the database.
-	 * - {string} _database.tables[0].id - The id of the DOM element in the HTML document.
-	 * - {string} _database.tables[0].name - The name of the table in the database.
-	 * @throws {Exception} [1000] - If the _database.name is not defined.
-	 * @throws {Exception} [1001] - If the _database.version is not defined.
-	 * @throws {Exception} [1002] - If the _database.tables is not defined.
-	 * @throws {Exception} [1003] - If the _database.tables[].id is not defined.
-	 * @throws {Exception} [1004] - If the _database.tables[].name is not defined.
-	 * @throws {Exception} [1005] - If the HTML element with id _database.tables[].id is not found.
+	* @param {object} _database - Associated database {object}.
+	* - {string} _database.name - The name of the database.
+	* - {number} _database.version - The version of the database.
+	* - {array[{object}]} _database.tables - The tables in the database.
+	* - {string} _database.tables[0].id - The id of the DOM element container in the HTML document.
+	* - {string} _database.tables[0].name - The name of the table in the database.
+	* - {string[]} _database.tables[0].types - The types of valid elements / element groups for this table (tables.name).
+	 * @throws {Exception} [1000] - If the _database.tables is not defined.
+	 * @throws {Exception} [1001] - If the _database.tables[].id is not defined.
+	 * @throws {Exception} [1002] - If the HTML element with id _database.tables[].id is not found.
 	 */
 	constructor(_database)
 	{
-		if (_database.name === undefined) { throw new Exception('_database.name{string} is not defined', 1000); }
-		if (_database.version === undefined) { throw new Exception('_database.version{number} is not defined', 1001); }
-		if (_database.tables === undefined) { throw new Exception('_database.tables{array} is not defined', 1002); }
-
-		this.databaseName = _database.name;
-		this.databaseVersion = _database.version;
-		this.databaseTables = _database.tables;
-
-		this.databaseTables.forEach((table) =>
+		if (_database.tables === undefined) { throw new Exception('_database.tables{array} is not defined', 1000); }
+		this.elementContainers = _database.tables;
+		this.elementContainers.forEach((container) =>
 		{
-			if (table.skip === true) { return; }
-			if (table.id === undefined) { throw new Exception('_database.tables[].id{string} is not defined', 1003); }
-			if (table.name === undefined) { throw new Exception('_database.tables[].name{string} is not defined', 1004); }
-			table.container = document.getElementById(table.id);
-			if (table.container === null) { throw new Exception('HTML element with id "' + table.id + '" not found', 1005); }
+			if (container.skip === true) { return; }
+			if (container.id === undefined) { throw new Exception('_database.tables[].id{string} is not defined', 1001); }
+			container.element = document.getElementById(container.id);
+			if (container.element === null) { throw new Exception('HTML element with id "' + container.id + '" not found', 1002); }
 		});
 
-		this.tableTypes = _database.types;
+		this.elementTypes = _database.types;
 		
 		this.DOMHandler = new DOMHandler();
 		this.DBHandler = new DBHandler(_database);
@@ -74,14 +59,14 @@ class ContentManager
 		if (_type === undefined) { throw new Exception('_type{string} is not defined', 1100); }
 		if (_name === undefined) { throw new Exception('_name{string} is not defined', 1101); }
 
-		let table = this._getTableByType(_type);
+		let elementContainer = this._getContainerByType(_type);
 		let directParent = this.DOMHandler.getElementById(_directParentId);
 
-		const newElement = this.DOMHandler.createElementByType(_type, _name, table.container, directParent);
+		const newElement = this.DOMHandler.createElementByType(_type, _name, elementContainer.element, directParent);
 
 		if (newElement === null || newElement === undefined) { throw new Exception('Could not create new element', 1102); }
 
-		this.DBHandler.addToDatabase({ id: newElement.id, name: newElement.name, type: newElement.type, parentID: newElement.parentID, method: newElement.method }, table.name);
+		this.DBHandler.addToDatabase({ id: newElement.id, name: newElement.name, type: newElement.type, parentID: newElement.parentID, method: newElement.method }, elementContainer.name);
 		
 		return newElement.id;
 	}
@@ -92,16 +77,16 @@ class ContentManager
 	 */
 	restoreAllElements()
 	{
-		this.databaseTables.forEach((table) =>
+		this.elementContainers.forEach((container) =>
 		{
-			this.DBHandler.getManyFromDatabase(table.name, null, {'structure' : 1, 'id' : 1})
+			this.DBHandler.getManyFromDatabase(container.name, null, {'structure' : 1, 'id' : 1})
 			.then((result) =>
 			{
 				result.forEach((element) =>
 				{
 					var parent = null;
-					if (element.parentID !== table.id) { parent = this.DOMHandler.getElementById(element.parentID); }
-					this.DOMHandler.createElementByType(element.type, element.name, table.container, parent, element.id, element.method);
+					if (element.parentID !== container.id) { parent = this.DOMHandler.getElementById(element.parentID); }
+					this.DOMHandler.createElementByType(element.type, element.name, container.element, parent, element.id, element.method);
 				});
 			});
 		});
@@ -117,28 +102,28 @@ class ContentManager
 	/**
 	 * @private Checks the _type parameter against the types{array}.
 	 * @param {string} _type - The _type to be checked in types{object}.
-	 * @return {object} The table that contains to the _type.
+	 * @return {object} The elementContainer that contains to the _type.
 	 * @throws {Exception} [1900] - If the _type is no valid element of types{object}.
-	 * @throws {Exception} [1901] - If the _database.tables[].name is not defined.
+	 * @throws {Exception} [1901] - If the elementContainer is not defined.
 	 */
-	_getTableByType(_type)
+	_getContainerByType(_type)
 	{
-		let tableName = null;
-		if (this.tableTypes.hasOwnProperty(_type)) { tableName = this.tableTypes[_type]; }
-		if (tableName === null) { throw new Exception('_type: "' + _type + '" is no valid element of types[' + Object.keys(this.tableTypes)+ ']', 1900); }
+		let containerName = null;
+		if (this.elementTypes.hasOwnProperty(_type)) { containerName = this.elementTypes[_type]; }
+		if (containerName === null) { throw new Exception('_type: "' + _type + '" is no valid element of types[' + Object.keys(this.elementTypes)+ ']', 1900); }
 
-		let table = null;
-		this.databaseTables.forEach((databaseTable) =>
+		let elementContainer = null;
+		this.elementContainers.forEach((container) =>
 		{
-			if (tableName === databaseTable.name)
+			if (containerName === container.name)
 			{
-				table = databaseTable;
+				elementContainer = container;
 				return;
 			}
 		});
-		if (table === null) { throw new Exception('"' + tableName + '" is not defined in the _database.tables[].name{string}', 1901); }
+		if (elementContainer === null) { throw new Exception('"' + containerName + '" is not defined in the elementContainer{string}', 1901); }
 
-		return table;
+		return elementContainer;
 	}
 	//#endregion
 }
